84f795ebc7e428428be5b570b2e8481ce01efcec
apparenelle <apparenelle@gmail.com>
2022-09-18 13:48:33 -0400

    flex upgrade
    -better understanding of flex
    -organized navbar

--- app/src/Nav.js
+++ app/src/Nav.js
@@ -5,19 +5,17 @@ import companyLogo from './assets/generic_logo.jpeg';
 function NavBar() {
     return(
         <div class="nav-flex-container">
-            <div class="nav-logo-flex-item">
+            <div class="nav-logo-flex-container">
                 <img class="nav-logo" src={companyLogo} />
             </div>
             
-            <div class="nav-link-flex-item">
-                <div>
-                    <ul class="nav-links">
-                        <li><a href='#'>Home</a></li>
-                        <li><a href='#'>Projects</a></li>
-                        <li><a href='#'>About Me</a></li>
-                        <li><a href='#'>Donate</a></li>
-                    </ul>
-                </div>
+            <div class="nav-link-flex-container">
+                <ul class="nav-links">
+                    <li><a href='#'>Home</a></li>
+                    <li><a href='#'>Projects</a></li>
+                    <li><a href='#'>About Me</a></li>
+                    <li><a href='#'>Donate</a></li>
+                </ul>
             </div>           
         </div>
     );    
