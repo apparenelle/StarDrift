f107f78aa146ebb388325ae2b88385d41cb1fd65
apparenelle <apparenelle@gmail.com>
2022-09-18 16:13:53 -0400

    update Sunday
    -added news bar
    -still buggy need to work out how to create and add elements in react

--- app/src/Nav.js
+++ app/src/Nav.js
@@ -1,24 +1,40 @@
 //import all images and documents and us js to call them.
 import './App.css'
 import companyLogo from './assets/generic_logo.jpeg';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
 
 function NavBar() {
     return(
-        <div class="nav-flex-container">
-            <div class="nav-logo-flex-container">
-                <img class="nav-logo" src={companyLogo} />
+        <div className="upper-sectional">
+            <div className="nav-flex-container">
+                <div className="nav-logo-flex-container">
+                    <img className="nav-logo" src={companyLogo} />
+                </div>
+                
+                <div className="nav-link-flex-container">
+                    <ul className="nav-links">
+                        <li><a href='#'>Home</a></li>
+                        <li><a href='#'>Projects</a></li>
+                        <li><a href='#'>About Me</a></li>
+                        <li><a href='#'>Donate</a></li>
+                    </ul>
+                </div>           
             </div>
-            
-            <div class="nav-link-flex-container">
-                <ul class="nav-links">
-                    <li><a href='#'>Home</a></li>
-                    <li><a href='#'>Projects</a></li>
-                    <li><a href='#'>About Me</a></li>
-                    <li><a href='#'>Donate</a></li>
-                </ul>
-            </div>           
-        </div>
-    );    
+            <div className="nav-news-container" id="news-drop" onLoad={grabNews()}>
+
+            </div>
+        </div>  
+    );
+}
+
+function grabNews() {
+    // console.log("News was called");
+    // const news = React.createElement("p");
+    // news.innerText = "This is the news";
+    // React.getElementsById("news-drop").appendChild(news);
 }
 
 
