7f0ec5e4638546d555910c782e4de477c1bbda68
apparenelle <apparenelle@gmail.com>
2022-09-21 18:43:06 -0400

    Upgrades
    -small changes

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
-import Bar from './Nav.js'
+import Bar from './Nav.js';
+import {First} from './First.js'
 import './App.css';
 
 function App() {
   return ( 
     <div className="App">
-      < Bar/>
+      <Bar/>
+      <First />
+
       <h1>Welcome to my digital portfolio!</h1>
       <h2>It is {new Date().toLocaleTimeString()} in New York!</h2>
     </div>
---
+++ app/src/First.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+
+export class First extends React.Component {
+    render(){
+        return (
+            <div className='first-sectional'>
+                <h1></h1>
+            </div>
+        );
+    }
+}
+
