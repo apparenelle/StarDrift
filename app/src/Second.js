b7cbdeb874c8bb12500fc712bb0d7745672ba497
apparenelle <apparenelle@gmail.com>
2022-09-28 16:10:11 -0400

    Skills
    -fitted skills
    -added gifs
    ?need to localize gifs
    ?need to solve illegibility of text

--- app/src/Second.js
+++ app/src/Second.js
@@ -7,12 +7,11 @@ export class Second extends React.Component{
             <div className='second-sectional'>
                 <h2>Skills</h2>
                 <div className='skills-flex-container'>
-                    <div className='skills-flex-item'>
-                        <a href='#'>Trading</a>
-                        <a href='#'>Coding</a>
-                        <a href='#'>Engineering</a>
-                        <a href='#'>Photography</a>
-                    </div>
+                    {/* skills-flex-container is also a flex item */}
+                        <a id='skills-trading' href='#'><b>Trading</b></a>
+                        <a id='skills-coding' href='#'>Coding</a>
+                        <a id='skills-engineering' href='#'>Engineering</a>
+                        <a id='skills-photography' href='#'>Photography</a>
                 </div>
 
             </div>
