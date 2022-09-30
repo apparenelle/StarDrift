8c086679978fa3125b8403f57d926511ffc7b822
apparenelle <apparenelle@gmail.com>
2022-09-30 00:06:05 -0400

    tweaks

--- app/src/Second.js
+++ app/src/Second.js
@@ -8,7 +8,13 @@ export class Second extends React.Component{
                 <h2>Skills</h2>
                 <div className='skills-flex-container'>
                     {/* skills-flex-container is also a flex item */}
-                        <a id='skills-trading' href='#'><b>Trading</b></a>
+                        <a href='#'>
+                            <div className='skills-trading-image'></div>
+                            <div className='skills-trading-label'><h5>Trading</h5></div>
+                        </a>
+                        
+                        
+                        
                         <a id='skills-coding' href='#'>Coding</a>
                         <a id='skills-engineering' href='#'>Engineering</a>
                         <a id='skills-photography' href='#'>Photography</a>
