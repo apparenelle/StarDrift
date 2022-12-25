cf2d8e303cc230fea6e30d3849ffcc28a7d0c512
apparenelle <apparenelle@gmail.com>
2022-12-25 15:54:42 -0500

    tweaks
    -some minor changes here and there

--- app/src/Sandwich.js
+++ app/src/Sandwich.js
@@ -34,8 +34,8 @@ export function Sandwich() {
 
 
 function openMobileNav(){
-    document.getElementById("myNav").style.width = "30%";
-    document.getElementById("root").style.marginLeft = "30%";
+    document.getElementById("myNav").style.width = "25%";
+    document.getElementById("root").style.marginLeft = "25%";
     document.getElementById("root").style.transition = "0.5s";
 
     console.log("nav was opened");
